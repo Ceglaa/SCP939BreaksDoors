8db2c906a545333b444a3f5475dbe1306d025a52
Ceglaa <84150438+Ceglaa@users.noreply.github.com>
2021-08-30 16:38:31 +0200

    Delete Config.cs

--- Config.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SCP939BreaksDoors
-{
-    using Exiled.API.Interfaces;
-    public sealed class Config : IConfig
-    {
-        public bool IsEnabled { get; set; } = true;
-        public bool Does939DestroyCheckpoints { get; private set; } = false;
-    }
-}
