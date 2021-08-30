706a2c667dbfc36b7b142a9a136940b26f9dd94b
Ceglaa <84150438+Ceglaa@users.noreply.github.com>
2021-08-30 16:37:59 +0200

    Add files via upload
    UwU

---
+++ Config.cs
@@ -0,0 +1,9 @@
+﻿namespace SCP939BreaksDoors
+{
+    using Exiled.API.Interfaces;
+    public sealed class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Does939DestroyCheckpoints { get; private set; } = false;
+    }
+}
